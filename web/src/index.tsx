fec388729c299ed13715bae61fbdc1cd121be475
kibirisu <szymon@wabisabi.cafe>
2025-10-23 17:31:11 +0200

    Add react router

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/foo",
+    element: <h1>bar</h1>
+  }
+]);
 
 const rootEl = document.getElementById('root');
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
   root.render(
     <React.StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </React.StrictMode>,
   );
 }
