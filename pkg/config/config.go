6c77b4096ee1ffbffdbced4e2608ee800e0e6e48
kibirisu <szymon@wabisabi.cafe>
2025-10-26 18:39:53 +0100

    Allow reading both envfile and env vars into configuration

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -7,20 +7,23 @@ import (
 )
 
 type Config struct {
-	AppEnv      string `mapstructure:"app_env"`
-	DatabaseUrl string `mapstructure:"database_url"`
+	AppEnv      string `mapstructure:"appenv"`
+	DatabaseUrl string `mapstructure:"databaseurl"`
 }
 
 func GetConfig() *Config {
 	viper.SetDefault("AppEnv", "prod")
-	viper.SetDefault("AppEnv", "postgres://postgres@localhost:5432/dev?sslmode=disable")
+	viper.SetDefault("DatabaseUrl", "postgres://postgres@localhost:5432/dev?sslmode=disable")
+	viper.RegisterAlias("AppEnv", "app_env")
+	viper.RegisterAlias("DatabaseUrl", "database_url")
 	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+
 	if err := viper.ReadInConfig(); err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
 	var config Config
-	err := viper.Unmarshal(&config)
-	if err != nil {
+	if err := viper.Unmarshal(&config); err != nil {
 		log.Fatal(err)
 	}
 	return &config
