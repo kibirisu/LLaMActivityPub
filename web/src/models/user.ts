f9b8c618541bcd97fedb03584dc5dfc0ccc57060
kibirisu <szymon@wabisabi.cafe>
2025-11-02 16:19:37 +0100

    Web: cleanup

--- web/src/models/user.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-  username: string;
-  password_hash: string;
-  bio?: string | null;
-  followers_count?: number | null;
-  following_count?: number | null;
-  is_admin?: boolean | null;
-}
