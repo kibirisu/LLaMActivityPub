f5370f74939aee4003abfca2261fed72cf2d83af
Jan Tacakiewicz <jan.tacakiewicz@protonmail.com>
2025-11-13 21:08:31 +0100

    web: stc: components: feed: added comment view

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import App from './App';
+import CommentView from './components/feed/CommentView';
 import UserProfile from './components/profile/UserProfile';
 
 const router = createBrowserRouter([
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
   {
     path: '/foo',
     element: <h1>bar</h1>,
+  },
+  {
+    path: '/post/:postId',
+    element: <CommentView />,
   },
 ]);
 
